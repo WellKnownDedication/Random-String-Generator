de88b89c9b13a382a4323666033e7e98d63e9d2d
Bohdan Krushelnytskyi <wellknowndedication@proton.me>
2024-07-03 16:49:22 +0300

    Fixed input overflow problem

--- main/main.cpp
+++ main/main.cpp
@@ -15,11 +15,12 @@ struct randomString
 
 int getStrLen()
 {
+    std::cout << "Write desired length of a string: ";
 
-    std::cout << "Write desired length: ";
-    int len{0};
+    int len{-1};
     std::cin >> len;
-    if (len < 0 or len > 32)
+
+    if (!(len > 0) and !(len < 32))
     {
         std::cout << "Invalid value for length, try values from 1 to 32";
         len = getStrLen();
@@ -30,10 +31,12 @@ int getStrLen()
 bool useCharType(std::string charType)
 {
     std::cout << "Do you wish to use " << charType << " in string(y/n)";
-    char result;
-    std::cin >> result;
+    std::string result;
+    std::getline(std::cin >> std::ws, result);
 
-    switch (result)
+    char ch = result[0];
+
+    switch (ch)
     {
     case 'Y':
     case 'y':
@@ -55,7 +58,7 @@ int getRand(int min, int max)
     return random_value;
 }
 
-char getChar(randomString str)
+std::string getStr(randomString str)
 {
     constexpr int asciiLen{126 - 33};
     char asciiList[asciiLen]; // all readable characters from ascii
@@ -63,22 +66,22 @@ char getChar(randomString str)
 
     if (str.symbols)
     {
-        for (int i = 33; i <= 47; ++i) // 47-33=14
+        for (int i = 33; i <= 47; ++i)
         {
             asciiList[k] = i;
             k++;
         }
-        for (int i = 58; i <= 64; ++i) // 64-58=6
+        for (int i = 58; i <= 64; ++i)
         {
             asciiList[k] = i;
             k++;
         }
-        for (int i = 91; i <= 96; ++i) // 96-91=5
+        for (int i = 91; i <= 96; ++i)
         {
             asciiList[k] = i;
             k++;
         }
-        for (int i = 123; i <= 126; ++i) // 126-123=3
+        for (int i = 123; i <= 126; ++i)
         {
             asciiList[k] = i;
             k++;
@@ -87,7 +90,7 @@ char getChar(randomString str)
 
     if (str.numbers)
     {
-        for (int i = 48; i <= 57; ++i) // 57-48=10
+        for (int i = 48; i <= 57; ++i)
         {
             asciiList[k] = i;
             k++;
@@ -96,7 +99,7 @@ char getChar(randomString str)
 
     if (str.upperLetters)
     {
-        for (int i = 65; i <= 90; ++i) // 122-97=25
+        for (int i = 65; i <= 90; ++i)
         {
             asciiList[k] = i;
             k++;
@@ -112,7 +115,14 @@ char getChar(randomString str)
         }
     }
 
-    return asciiList[getRand(0, k)];
+    std::string finalStr{""};
+    for (int i{0}; i < str.len; i++)
+    {
+        char c{asciiList[getRand(0, k)]};
+        finalStr = finalStr + c;
+    }
+
+    return finalStr;
 }
 
 int main()
@@ -128,17 +138,16 @@ int main()
         str.letters = useCharType("letters");
         str.upperLetters = useCharType("upper letters");
 
-        for (int i{0}; i < str.len; i++)
-        {
-            str.finalString = str.finalString + getChar(str);
-        }
+        str.finalString = getStr(str);
 
         std::cout << "\nHere is created string: " << str.finalString << "\n";
         std::cout << "\nDo you wish to continue(y/n): ";
-        char result;
-        std::cin >> result;
+        std::string result;
+        std::getline(std::cin >> std::ws, result);
 
-        switch (result)
+        char ch = result[0];
+
+        switch (ch)
         {
         case 'Y':
         case 'y':
